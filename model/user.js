19b662f6b2933f8ce402ee0aba83908d5414ce10
RajGhaznavi <rajghaznavi7@gmail.com>
2024-05-03 00:32:53 +0530

    first commit

---
+++ model/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+
+  isCameraman: {
+    type: Boolean,
+  },
+  excludedIntervals: [
+    {
+      start: { type: Date },
+      end: { type: Date },
+    },
+  ],
+});
+
+const User = mongoose.model("Users", userSchema);
+
+module.exports = User;
